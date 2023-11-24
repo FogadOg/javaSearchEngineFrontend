b6427ca6c5311f750bf01a8e6cf48b7e2c1cd8a1
FogadOg <fogadm06@gmail.com>
2023-11-24 19:48:35 +0100

    adding frontend to search engine

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [websites, setWebsites] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/")
+      .then((res) => res.json())
+      .then((data) => {
+        setWebsites(data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }, []);
+
+  console.log(websites);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Websites</h1>
+      <ul>
+        {websites.map((website, index) => (
+          <li>
+            
+            <a href={website.url}>{website.pageTitle}</a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
